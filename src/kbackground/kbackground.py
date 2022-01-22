5eceb6af98d924ea6e477b89558c5ec8132ac70c
Christina Hedges <christina.l.hedges@nasa.gov>
2022-01-21 17:04:03 -0800

    sped up to increase resolution

--- src/kbackground/kbackground.py
+++ src/kbackground/kbackground.py
@@ -41,15 +41,15 @@ class Estimator:
     row: np.ndarray
     column: np.ndarray
     flux: np.ndarray
-    tknotspacing: int = 10
-    xknotspacing: int = 20
+    tknotspacing: int = 5
+    xknotspacing: int = 10
     mask: Optional[np.ndarray] = None
 
     def __post_init__(self):
-        log.debug(
+        log.info(
             f"new `kbackground` Object. tknotspacing:{self.tknotspacing}, xknotspacing:{self.tknotspacing}"
         )
-        log.debug(f"ntimes x npixels : {self.flux.shape}")
+        log.info(f"ntimes x npixels : {self.flux.shape}")
         s = np.argsort(self.time)
         self.time, self.flux = self.time[s], self.flux[s]
         self.xknots = np.arange(20, 1108, self.xknotspacing)
@@ -60,7 +60,11 @@ class Estimator:
             self.tknots = np.arange(self.time[0], self.time[-1], self.tknotspacing)
         time_corr = np.nanpercentile(self.flux, 20, axis=1)[:, None]
         med_flux = np.median(self.flux - time_corr, axis=0)[None, :]
+
         f = self.flux - med_flux
+        f -= np.median(f)
+
+        self.bad_frames = []
         if self.mask is None:
             # Mask out pixels that are particularly bright.
             self.mask = (f - time_corr).std(axis=0) < 500
@@ -70,6 +74,12 @@ class Estimator:
             # self.mask=(med_flux[0] - np.percentile(med_flux, 20)) < 30
             self.mask &= ~sigma_clip(med_flux[0]).mask
             self.mask &= ~sigma_clip(np.std(f - time_corr, axis=0)).mask
+
+        ratio = (self.flux[:100].mean(axis=0) - self.flux[-100:].mean(axis=0)) / (
+            self.flux.mean(axis=0)
+        )
+        self.mask &= ~sigma_clip(np.ma.masked_array(ratio, ~self.mask), sigma=2.5).mask
+
         self.unq_row = np.unique(self.row)
         log.debug(
             f"unq_row : {self.unq_row.min()} ... {self.unq_row.max()} ({len(self.unq_row)} unique rows)"
@@ -86,57 +96,95 @@ class Estimator:
             )
         log.debug("Binned flux data")
         log.debug(f"`bf` {self.bf.shape}")
-        log.debug("Making A")
-        A1 = self._make_A(self.unq_row, self.time)
-        log.debug("Made A")
-        self.bad_frames = (
-            np.where(
-                sigma_clip(
-                    np.diff(np.nanmean(self.bf, axis=0)),
-                    sigma_upper=3,
-                    sigma_lower=1e8,
-                ).mask
-            )[0]
-            + 1
+
+        breaks = (
+            np.where(np.diff(self.time) > 10 * np.median(np.diff(self.time)))[0] + 1
         )
-        log.debug(f"{len(self.bad_frames)}/{len(self.time)} bad frames")
+        log.info(f"{len(breaks)} break points")
+        log.debug(f"{breaks}")
 
-        if len(self.bad_frames) > 0:
-            log.debug("Building bad frame offsets")
-            badA = sparse.vstack(
-                [
-                    sparse.csr_matrix(
-                        (
-                            np.in1d(np.arange(len(self.time)), b)
-                            * np.ones(self.bf.shape, bool)
-                        ).ravel()
-                    )
-                    for b in self.bad_frames
-                ]
-            ).T
-            log.debug("Appending bad frame offsets")
-            self.A = sparse.hstack([A1, badA], "csr")
-            log.debug("Appended bad frame offsets")
-        else:
-            self.A = A1.tocsr()
-        prior_sigma = np.ones(self.A.shape[1]) * 100
-        k = np.isfinite(self.bf.ravel())
-        log.debug("Creating `sigma_w_inv`")
-        sigma_w_inv = self.A[k].T.dot(self.A[k]) + np.diag(1 / prior_sigma ** 2)
-        log.debug("Created `sigma_w_inv`")
-        log.debug("Creating `B`")
-        B = self.A[k].T.dot(self.bf.ravel()[k])
-        log.debug("Created `B`")
-        log.debug("Solving for `w`")
-        # self.w = np.linalg.solve(sigma_w_inv, B)
-        self.w = cho_solve(cho_factor(sigma_w_inv), B)
-        log.debug("Solved for `w`")
+        def get_masks(xlim1, xlim2):
+            x = np.arange(xlim1, xlim2, self.tknotspacing * 30)
+            a = np.max([x ** 0 * xlim1 - self.tknotspacing * 5, x], axis=0)
+            b = np.min([x ** 0 * xlim2, x + self.tknotspacing * 35], axis=0)
+            masks = np.asarray(
+                [np.in1d(self.time, self.time[a1:b1]) for a1, b1 in np.vstack([a, b]).T]
+            )
+            if masks.shape[0] >= 2:
+                masks[-2] |= masks[-1]
+            return masks[:-1]
 
-        self._model = self.A.dot(self.w).reshape(self.bf.shape)
-        log.debug("Building full model")
-        self.model = np.zeros((self.flux.shape)) * np.nan
-        for idx, u in enumerate(self.unq_row):
-            self.model[:, self.row == u] = self._model[idx][:, None]
+        time_masks = []
+        for xlim1, xlim2 in zip(
+            np.hstack([0, breaks]), np.hstack([breaks, len(self.time)])
+        ):
+            time_masks.append(get_masks(xlim1, xlim2))
+        time_masks = np.vstack(time_masks)
+
+        self.model = np.zeros_like(self.flux) * np.nan
+        self.weights = np.zeros_like(self.flux)
+        self._model = np.zeros_like(self.bf)
+        for tdx, tm in enumerate(time_masks):
+            log.info(f"Fitting model for segment {tdx + 1}/{len(time_masks)}")
+            log.debug(f"{tdx + 1}/{len(time_masks)} Making A")
+            A1 = self._make_A(self.unq_row, self.time[tm])
+            log.debug(f"{tdx + 1}/{len(time_masks)} Made A")
+            bad_frames = (
+                np.where(
+                    sigma_clip(
+                        np.diff(np.nanmean(self.bf[:, tm], axis=0)),
+                        sigma_upper=5,
+                        sigma_lower=1e8,
+                    ).mask
+                )[0]
+                + 1
+            )
+            log.debug(
+                f"{tdx + 1}/{len(time_masks)} {len(bad_frames)}/{len(self.time[tm])} bad frames"
+            )
+
+            if len(bad_frames) > 0:
+                log.debug(f"{tdx + 1}/{len(time_masks)} Building bad frame offsets")
+                badA = sparse.vstack(
+                    [
+                        sparse.csr_matrix(
+                            (
+                                np.in1d(np.arange(len(self.time[tm])), b)
+                                * np.ones(self.bf[:, tm].shape, bool)
+                            ).ravel()
+                        )
+                        for b in bad_frames
+                    ]
+                ).T
+                log.debug(f"{tdx + 1}/{len(time_masks)} Appending bad frame offsets")
+                self.A = sparse.hstack([A1, badA], "csr")
+                log.debug(f"{tdx + 1}/{len(time_masks)} Appended bad frame offsets")
+            else:
+                self.A = A1.tocsr()
+            prior_sigma = np.ones(self.A.shape[1]) * 100
+            k = np.isfinite(self.bf[:, tm].ravel())
+            log.debug(f"{tdx + 1}/{len(time_masks)} Creating `sigma_w_inv`")
+            sigma_w_inv = self.A[k].T.dot(self.A[k]) + np.diag(1 / prior_sigma ** 2)
+            log.debug(f"{tdx + 1}/{len(time_masks)} Created `sigma_w_inv`")
+            log.debug(f"{tdx + 1}/{len(time_masks)} Creating `B`")
+            B = self.A[k].T.dot(self.bf[:, tm].ravel()[k])
+            log.debug(f"{tdx + 1}/{len(time_masks)} Created `B`")
+            log.debug(f"{tdx + 1}/{len(time_masks)} Solving for `w`")
+            # self.w = np.linalg.solve(sigma_w_inv, B)
+            self.w = cho_solve(cho_factor(sigma_w_inv), B)
+            log.debug(f"{tdx + 1}/{len(time_masks)} Solved for `w`")
+
+            self._model[:, tm] = self.A.dot(self.w).reshape(self.bf[:, tm].shape)
+            log.debug(f"{tdx + 1}/{len(time_masks)} Building full model")
+            model = np.zeros((tm.sum(), self.flux.shape[1])) * np.nan
+            for idx, u in enumerate(self.unq_row):
+                model[:, self.row == u] = self._model[:, tm][idx][:, None]
+            self.model[tm] = model
+            self.weights[tm] = np.ones(model.shape)
+            self.bad_frames.append(np.where(tm)[0][bad_frames])
+        if len(self.bad_frames) != 0:
+            self.bad_frames = np.hstack(self.bad_frames)
+        self.model = self.model / self.weights
         self.model -= np.median(self.model)
         log.debug("Built")
 
@@ -220,7 +268,7 @@ class Estimator:
             np.asarray(
                 dmatrix(
                     "bs(x, knots=knots, degree=3, include_intercept=True)",
-                    {"x": np.hstack([0, x, 1400]), "knots": self.xknots},
+                    {"x": np.hstack([0, x, 1400]), "knots": self.xknots + 1e-10},
                 )
             )
         )[1:-1]
@@ -230,10 +278,13 @@ class Estimator:
             np.asarray(
                 dmatrix(
                     "bs(x, knots=knots, degree=3, include_intercept=True)",
-                    {"x": t, "knots": self.tknots},
+                    {
+                        "x": np.hstack([self.time[0], t, self.time[-1]]),
+                        "knots": self.tknots + 1e-10,
+                    },
                 )
             )
-        )
+        )[1:-1]
         t_spline = t_spline[:, np.asarray((t_spline.sum(axis=0) != 0))[0]]
         log.debug(f"Made `t_spline` {t_spline.shape}")
         X = (
